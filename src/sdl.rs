488e13b4eefe6b3f37c0185a227fc3f395666fdb
Maarten de Vries <maarten@de-vri.es>
2020-01-03 18:00:54 +0100

    Hide and potentially destroy closed windows.

--- src/sdl.rs
+++ src/sdl.rs
@@ -1,3 +1,5 @@
+use sdl2::event::Event;
+use sdl2::event::WindowEvent;
 use sdl2::pixels::PixelFormatEnum;
 use sdl2::render::Canvas;
 use sdl2::render::Texture;
@@ -13,7 +15,7 @@ use crate::PixelFormat;
 pub struct Context {
 	video: sdl2::VideoSubsystem,
 	events: sdl2::EventPump,
-	windows: Vec<Arc<Mutex<WindowInner>>>,
+	windows: Vec<(u32, Arc<Mutex<WindowInner>>)>,
 }
 
 struct WindowInner {
@@ -47,28 +49,34 @@ impl Context {
 			.build()
 			.map_err(|e| format!("failed to create window {:?}: {}", name, e))?;
 
+		let id = window.id();
+
 		let canvas = window.into_canvas().build().map_err(|e| format!("failed to create canvasr for window {:?}: {}", name, e))?;
 
 		let texture_creator = canvas.texture_creator();
 
-		let inner = Arc::new(Mutex::new(WindowInner {
+		let inner = WindowInner {
 			canvas,
 			texture_creator,
 			texture: None,
-		}));
+		};
+		let inner = Arc::new(Mutex::new(inner));
 
-		self.windows.push(inner.clone());
+		self.windows.push((id, inner.clone()));
 
 		Ok(Window { inner })
 	}
 
 	pub fn run(&mut self) -> Result<(), String> {
 		loop {
-			for event in self.events.poll_iter() {
-				// TODO
+			while let Some(event) = self.events.poll_event() {
+				match event {
+					Event::Window { window_id, win_event, .. } => self.handle_window_event(window_id, win_event)?,
+					_ => (),
+				}
 			}
 
-			for window in &self.windows {
+			for (_id, window) in &self.windows {
 				let mut window = window.lock().unwrap();
 				let texture = window.texture.take();
 				if let Some((texture, image_size)) = texture {
@@ -76,11 +84,30 @@ impl Context {
 					window.canvas.copy(&texture, image_size.clone(), viewport)
 						.map_err(|e| format!("failed to copy data to window: {}", e))?;
 					window.texture = Some((texture, image_size));
+					window.canvas.window_mut().show();
 				}
 				window.canvas.present();
 			}
 
 			std::thread::sleep(std::time::Duration::from_millis(1000 / 60));
+		}
+	}
+
+	fn handle_window_event(&mut self, window_id: u32, event: WindowEvent) -> Result<(), String> {
+		match event {
+			WindowEvent::Close => self.close_window(window_id),
+			_ => (),
+		}
+		// TODO;
+		Ok(())
+	}
+
+	fn close_window(&mut self, window_id: u32) {
+		if let Some(index) = self.windows.iter().position(|(id, _)| *id == window_id) {
+			let (_id, window) = self.windows.remove(index);
+			let mut window = window.lock().unwrap();
+			window.texture = None;
+			window.canvas.window_mut().hide();
 		}
 	}
 }
