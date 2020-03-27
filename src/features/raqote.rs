128d66ba5614783003baeaea3ba933c9ec458a5b
Maarten de Vries <maarten@de-vri.es>
2020-03-27 17:50:13 +0100

    Divide raqote data by alpha channel, to undo premultiplication.

--- src/features/raqote.rs
+++ src/features/raqote.rs
@@ -2,6 +2,17 @@ use crate::ImageData;
 use crate::ImageInfo;
 use crate::PixelFormat;
 
+fn divide_by_alpha(data: &mut [u8]) {
+	for i in 0..(data.len() / 4) {
+		let i = i * 4;
+		if data[i + 3] != 0  && data[i + 3] != 255 {
+			data[i + 0] = (u16::from(data[i + 0]) * 255 / u16::from(data[i + 3])) as u8;
+			data[i + 1] = (u16::from(data[i + 1]) * 255 / u16::from(data[i + 3])) as u8;
+			data[i + 2] = (u16::from(data[i + 2]) * 255 / u16::from(data[i + 3])) as u8;
+		}
+	}
+}
+
 impl ImageData for raqote::DrawTarget {
 	fn info(&self) -> Result<ImageInfo, String> {
 		if self.width() < 0 || self.height() < 0 {
@@ -15,7 +26,9 @@ impl ImageData for raqote::DrawTarget {
 		let length = self.get_data_u8().len();
 		let data = Box::into_raw(self.into_vec().into_boxed_slice()) as *mut u8;
 		unsafe {
-			Box::from_raw(std::slice::from_raw_parts_mut(data, length))
+			let mut data = Box::from_raw(std::slice::from_raw_parts_mut(data, length));
+			divide_by_alpha(&mut data);
+			data
 		}
 	}
 }
@@ -26,7 +39,9 @@ impl ImageData for &'_ raqote::DrawTarget {
 	}
 
 	fn data(self) -> Box<[u8]> {
-		Box::from(self.get_data_u8())
+		let mut data = Box::from(self.get_data_u8());
+		divide_by_alpha(&mut data);
+		data
 	}
 }
 
@@ -42,7 +57,9 @@ impl<'a> ImageData for raqote::Image<'a> {
 	fn data(self) -> Box<[u8]> {
 		let data = self.data.as_ptr() as *const u8;
 		unsafe {
-			Box::from(std::slice::from_raw_parts(data, self.data.len() * 4))
+			let mut data = Box::from(std::slice::from_raw_parts(data, self.data.len() * 4));
+			divide_by_alpha(&mut data);
+			data
 		}
 	}
 }
