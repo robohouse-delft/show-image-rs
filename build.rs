46a9610502050c69db189cc463e961b86dd5cf94
Maarten de Vries <maarten@de-vri.es>
2020-08-27 23:21:48 +0200

    Prevent unneeded rebuilding because of build script.

--- build.rs
+++ build.rs
@@ -3,5 +3,6 @@ use rustc_version::{version_meta, Channel};
 fn main() {
 	if version_meta().unwrap().channel <= Channel::Nightly {
 		println!("cargo:rustc-cfg=nightly");
+		println!("cargo:rerun-if-changed=build.rs");
 	}
 }
