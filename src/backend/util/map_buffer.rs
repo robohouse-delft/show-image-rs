e23a67270c18155630d6dcb4fc987dce4c0a982c
Maarten de Vries <maarten@de-vri.es>
2020-09-19 14:07:05 +0200

    Add utilities to block while mapping a buffer.

---
+++ src/backend/util/map_buffer.rs
@@ -0,0 +1,58 @@
+use std::future::Future;
+use std::pin::Pin;
+use std::task::Context;
+use std::task::Poll;
+use std::task::RawWaker;
+use std::task::RawWakerVTable;
+use std::task::Waker;
+
+/// A vtable with all no-ops.
+static NULL_WAKER_VTABLE: RawWakerVTable = RawWakerVTable::new(
+	|_| raw_null_waker(),
+	|_| (),
+	|_| (),
+	|_| (),
+);
+
+/// Create a raw null waker that does nothing.
+fn raw_null_waker() -> RawWaker {
+	RawWaker::new(std::ptr::null(), &NULL_WAKER_VTABLE)
+}
+
+/// Create a null waker that does nothing.
+fn null_waker() -> Waker {
+	unsafe {
+		Waker::from_raw(raw_null_waker())
+	}
+}
+
+/// Synchronously wait for a buffer to be mappable.
+fn wait_for_buffer<'a>(device: &wgpu::Device, buffer: wgpu::BufferSlice<'a>, map_mode: wgpu::MapMode) -> Result<(), wgpu::BufferAsyncError> {
+	let mut future = buffer.map_async(map_mode);
+	let waker = null_waker();
+
+	loop {
+		let future = Pin::new(&mut future);
+		match future.poll(&mut Context::from_waker(&waker)) {
+			Poll::Ready(x) => return x,
+			Poll::Pending => device.poll(wgpu::Maintain::Wait),
+		}
+	}
+}
+
+/// Synchronously map a buffer for read access.
+///
+/// This will internally call [`wgpu::Device::poll()`] until the buffer is ready, and then map it.
+pub fn map_buffer<'a>(device: &wgpu::Device, buffer: wgpu::BufferSlice<'a>) -> Result<wgpu::BufferView<'a>, wgpu::BufferAsyncError> {
+	wait_for_buffer(device, buffer, wgpu::MapMode::Read)?;
+	Ok(buffer.get_mapped_range())
+}
+
+/// Synchronously map a buffer for write access.
+///
+/// This will internally call [`wgpu::Device::poll()`] until the buffer is ready, and then map it.
+#[allow(unused)]
+pub fn map_buffer_mut<'a>(device: &wgpu::Device, buffer: wgpu::BufferSlice<'a>) -> Result<wgpu::BufferViewMut<'a>, wgpu::BufferAsyncError> {
+	wait_for_buffer(device, buffer, wgpu::MapMode::Write)?;
+	Ok(buffer.get_mapped_range_mut())
+}
--- src/backend/util/mod.rs
+++ src/backend/util/mod.rs
@@ -1,10 +1,13 @@
 mod buffer;
 mod gpu_image;
+mod map_buffer;
 mod retain_mut;
 mod uniforms_buffer;
 
 pub use buffer::create_buffer_with_value;
 pub use gpu_image::GpuImage;
 pub use gpu_image::GpuImageUniforms;
+pub use map_buffer::map_buffer;
+pub use map_buffer::map_buffer_mut;
 pub use retain_mut::RetainMut;
 pub use uniforms_buffer::UniformsBuffer;
