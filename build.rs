e0d9c6b32932ea99654314c2014f24689d9707f5
Maarten de Vries <maarten@de-vri.es>
2020-09-27 17:32:56 +0200

    Make sure build script doesn't get rerun unneed outside of nightly.

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
 use rustc_version::{version_meta, Channel};
 
 fn main() {
+	println!("cargo:rerun-if-changed=build.rs");
 	if version_meta().unwrap().channel <= Channel::Nightly {
 		println!("cargo:rustc-cfg=nightly");
-		println!("cargo:rerun-if-changed=build.rs");
 	}
 }
